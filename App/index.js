b78a00355a3c5537f64943cd074e37befc954f92
Mudit Ameta <zeusdeux@gmail.com>
2018-07-01 01:33:20 +0200

    Add more css

--- App/index.js
+++ App/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Switch } from 'react-router-dom'
+import './app.css'
 
 export default function App({ links, routes }) {
   return (
