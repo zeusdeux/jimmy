ed209a0c8201d1f0c58cfdebc99e4ab094dde0b7
Mudit Ameta <zeusdeux@gmail.com>
2018-07-01 02:52:58 +0200

    Add some interactivity to show client side routing works as expected

--- App/Routes/home/:count/index.js
+++ App/Routes/home/:count/index.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 export default function HomeWithParam({ match }) {
-  return <h2>Received param: {match.params.count}</h2>
+  const count = Number.parseInt(match.params.count, 10)
+
+  return (
+    <>
+      <h2>Received param: {match.params.count}</h2>
+      <Link to={`/home/${Number.isNaN(count) ? 0 : count + 1}`}>Next</Link>
+    </>
+  )
 }
 
 HomeWithParam.propTypes = {
