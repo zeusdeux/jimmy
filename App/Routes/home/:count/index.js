d42d7ab31c7b83356567925efff309440a7a9c2c
Mudit Ameta <zeusdeux@gmail.com>
2018-07-01 02:29:40 +0200

    Rename :param to :count

--- App/Routes/home/:count/index.js
+++ App/Routes/home/:count/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 export default function HomeWithParam({ match }) {
-  return <h2>Received param: ${match.params.count}</h2>
+  return <h2>Received param: {match.params.count}</h2>
 }
 
 HomeWithParam.propTypes = {
